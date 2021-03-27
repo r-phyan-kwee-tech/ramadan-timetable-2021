f4aef470561033fec3076d10c656a2b62063e493
winhtaikaung <winhtaikaung28@gmail.com>
2021-03-27 10:52:52 +0630

    api middleware added

---
+++ src/api/type.d.ts
@@ -0,0 +1,12 @@
+
+type ApiReducerType = {
+    loading: boolean
+    data?: any
+    error?: string
+  }
+  
+  type ApiActionType = {
+    type: 'loading' | 'success' | 'error'
+    data?: any
+    error?: Error
+  }
