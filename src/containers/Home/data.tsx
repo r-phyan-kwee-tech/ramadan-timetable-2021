4a95ff5fb19c0ac4f642485661b1a842f1db0ad2
winhtaikaung <winhtaikaung28@gmail.com>
2021-03-28 08:44:14 +0630

    basic ui implemented

--- src/containers/Home/data.tsx
+++ src/containers/Home/data.tsx
@@ -1,9 +1,9 @@
 export const getCountry = (limit: number, page: number) => {
-  const fetchCountryQuery = `{  countries(limit: ${limit}, page: ${page}) {    data {      id      objectId      name      createdDate      
+  return `{  countries(limit: ${limit}, page: ${page}) {    data {      id      objectId      name      createdDate      
 updatedDate    }  }}`
 }
 
 export const getStates = (limit: number, page: number, countryId: string) => {
-  const fetchStatesQuery = `{  states(limit:  ${limit} , page:  ${page}, countryId:  "${countryId}") {    data {      id      objectId      nameMmUni      nameMmZawgyi
+  return `{  states(limit:  ${limit} , page:  ${page}, countryId:  "${countryId}") {    data {      id      objectId      nameMmUni      nameMmZawgyi
     countryId      createdDate      updatedDate    }  }}`
 }
